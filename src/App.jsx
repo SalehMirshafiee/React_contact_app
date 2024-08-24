02ef642e336963394dd2c3d94eae8670603cf322
Saleh Mirshafiee <seyed82saleh@gmail.com>
2024-08-24 16:45:34 +0330

    handling the inputs of the Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Contacts />
     </>
   );
 }
