fe996e67ad1c5f6b7ce291aea71748d3045dd320
Saleh Mirshafiee <seyed82saleh@gmail.com>
2024-08-24 19:40:13 +0330

    adding deleteHandler fun using id in contact.jsx and props it to contactItem.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -40,7 +40,11 @@ function Contacts() {
       email: "",
       phone: "",
     });
-    console.log(contacts);
+  };
+
+  const deleteHandler = (id) => {
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(newContacts);
   };
 
   return (
@@ -60,7 +64,7 @@ function Contacts() {
         <button onClick={addHandler}>Add Contact</button>
       </div>
       <div>{alert && <p>{alert}</p>}</div>
-      <ContactsList contacts={contacts} />
+      <ContactsList contacts={contacts} deleteHandler={deleteHandler} />
     </div>
   );
 }
